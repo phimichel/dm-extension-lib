767b134b39d11ebbb13dcf3dc98567aa3c0926dd
Philipp Michel <philipp.michel@easy-software.com>
2021-10-28 15:29:03 +0200

    Implementd first draft of getting exchange token

--- dm-extension-auth.js
+++ dm-extension-auth.js
@@ -1,40 +1,52 @@
-import * as Keycloak  from './keycloak.js'
+import * as k from "./keycloak.js";
 
-export function exchangeToken(token, authUrl, realm, clientId, clientSecret, subjectIssuer) {
-    var settings = {
-        async: true,
-        crossDomain: true,
-        url: authUrl,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        data: {
-            client_id: clientId,
-            client_secret: clientSecret,
-            grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
-            subject_token_type: "urn:ietf:params:oauth:token-type:access_token",
-            requested_token_type: "urn:ietf:params:oauth:token-type:refresh_token",
-            subject_token: token,
-            subject_issuer: subjectIssuer,
-        }
-    }
+//token, authUrl, realm, clientId, clientSecret, subjectIssuer
+export async function exchangeToken(options) {
+  var settings = {
+    async: true,
+    crossDomain: true,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
+  const params = {
+    client_id: options.clientId,
+    client_secret: options.clientSecret,
+    grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
+    subject_token_type: "urn:ietf:params:oauth:token-type:access_token",
+    requested_token_type: "urn:ietf:params:oauth:token-type:refresh_token",
+    subject_token: options.token,
+    subject_issuer: options.subjectIssuer,
+  };
 
-    const tokenExchangeAdapter = {
-        login(options) {
-            console.log("Login called")
-        }
-    }
+  const response = await fetch(options.authUrl, {
+    method: "POST",
+    body: new URLSearchParams(params),
+  });
 
-    // var keycloak = new Keycloak({
-    //     url: authUrl,
-    //     realm,
-    //     clientId,
-    //     clientSecret,
-    // });
-    var keycloak = new Keycloak({
-        adapter: tokenExchangeAdapter,
-    })
+  if (!response.ok) {
+    const message = `An error has occurred: ${response.status}`;
+    throw new Error(message);
+  }
+  return response.json();
 
-    keycloak.init()
+  // const tokenExchangeAdapter = {
+  //     login(options) {
+  //         console.log("Login called")
+  //     }
+  // }
+
+  // var keycloak = new Keycloak({
+  //     url: authUrl,
+  //     realm,
+  //     clientId,
+  //     clientSecret,
+  // });
+  // console.log(k)
+  // var keycloak = new Keycloak({
+  //     adapter: tokenExchangeAdapter,
+  // })
+
+  // keycloak.init()
 }
