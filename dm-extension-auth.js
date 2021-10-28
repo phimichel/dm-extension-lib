c1f42431feecf14cb3ed69be77b2ff44c558af71
Philipp Michel <philipp.michel@easy-software.com>
2021-10-28 14:33:37 +0200

    Trying to add keycloak lib

--- dm-extension-auth.js
+++ dm-extension-auth.js
@@ -1,3 +1,40 @@
-export function exchangeToken(token, url) {
-    console.log("Exchange token function called");
+import Keycloak, { KeycloakAdapter }  from './keycloak.js'
+
+export function exchangeToken(token, authUrl, realm, clientId, clientSecret, subjectIssuer) {
+    var settings = {
+        async: true,
+        crossDomain: true,
+        url: authUrl,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: {
+            client_id: clientId,
+            client_secret: clientSecret,
+            grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
+            subject_token_type: "urn:ietf:params:oauth:token-type:access_token",
+            requested_token_type: "urn:ietf:params:oauth:token-type:refresh_token",
+            subject_token: token,
+            subject_issuer: subjectIssuer,
+        }
+    }
+
+    const tokenExchangeAdapter = {
+        login(options) {
+            console.log("Login called")
+        }
+    }
+
+    // var keycloak = new Keycloak({
+    //     url: authUrl,
+    //     realm,
+    //     clientId,
+    //     clientSecret,
+    // });
+    var keycloak = new Keycloak({
+        adapter: tokenExchangeAdapter,
+    })
+
+    keycloak.init()
 }
